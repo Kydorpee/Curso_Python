04e8946301de0e2e126c6504456504a4fd9043d8
Kydorpe <lopedro45@gmail.com>
2026-05-20 20:47:15 -0300

    update

--- Au_003/ex001.py
+++ Au_003/ex001.py
@@ -1,4 +1,8 @@
-print("""
+import os
+escolha_cliente = 0
+
+def iniciar_app():
+    print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -6,21 +10,29 @@ print("""
 ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
 """)
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
+def coletar_escolha(x):
+    x = input('Escolha uma opção: ')
+    if x == '1':
+        print('Opção 1 - Cadastrar restaurante')
+    elif x == '2':
+        print('Opção 2 - Listar restaurante')
+    elif x == '3':
+        print('Opção 3 - Ativar restaurante')
+    elif x == '4':
+        print('Opção 4 - Sair')
+        sair_menu()
+    else:
+        print('Opção inválida!')
+        return x
+def sair_menu():
+    os.system('cls')
+    print('Saindo do menu...')
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
+    
+iniciar_app()
+coletar_escolha(escolha_cliente)
 
-opcao_escolhida = input('Escolha uma opção: ')
-
-if opcao_escolhida == '1':
-    print('Opção 1 - Cadastrar restaurante')
-elif opcao_escolhida == '2':
-    print('Opção 2 - Listar restaurante')
-elif opcao_escolhida == '3':
-    print('Opção 3 - Ativar restaurante')
-elif opcao_escolhida == '4':
-    print('Opção 4 - Sair')
-else:
-    print('Opção inválida!')
