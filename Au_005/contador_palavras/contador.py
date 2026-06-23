97d70c1b9061358429b6575ec90172c4e70c75d4
Kydorpe <lopedro45@gmail.com>
2026-06-23 18:33:09 -0300

    completo!

--- Au_005/contador_palavras/contador.py
+++ Au_005/contador_palavras/contador.py
@@ -2,11 +2,16 @@ frase = ''
 
 def receber_frase(frase):
     frase = input("Digite uma frase: ")
+    frase=tratar_frase(frase)
     return frase
 def contar_palavras(frase):
+    if not frase.strip():
+        return {}
     palavras = frase.split()
-    print(len(palavras))
-    print(palavras)
+    contagem = {}
+    for palavra in palavras:
+        contagem[palavra] = contagem.get(palavra,0)+1
+    return contagem
 def tratar_frase(frase):
     frase = frase.lower()
     caracter = "!@#$%^&*()-_=+[{]};:'\",<.>/?\\|`~"
--- Au_005/contador_palavras/main.py
+++ Au_005/contador_palavras/main.py
@@ -1,5 +1,15 @@
 from contador import *
 
 frase = receber_frase(frase)
-frase = tratar_frase(frase)
-contar_palavras(frase)
+if not frase:
+    print("Erro ao indentificar sua frase! tente novamente...")
+    input("")
+    frase = receber_frase(frase)
+else:
+    resultado = contar_palavras(frase)
+    if resultado:
+        print("Contagem de palavras:")
+        for palavra,quantidade in resultado.items():
+             print(f"{palavra}:{quantidade}")
+        else:
+            print("Nenhuma palavra foi localizada!")
