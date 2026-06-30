57e0b9a2352d6ac77ad0d4daf4b5275b018d90c6
Kydorpe <lopedro45@gmail.com>
2026-06-30 19:27:13 -0300

    nova Aula

---
+++ Au_006/restaurante.py
@@ -0,0 +1,12 @@
+class Restaurante:
+    nome = "" 
+    categoria = ""
+    status = False
+
+
+restaurante_teste = Restaurante()
+restaurante_teste.nome = 'Dogão'
+restaurante_teste.categoria = 'Cachorro-quente'
+
+
+print()
